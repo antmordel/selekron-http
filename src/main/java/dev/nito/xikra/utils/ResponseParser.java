cb94835a73c69f58519f9417a2adedc173325e52
Nito Moreno <thedownhiller@gmail.com>
2022-02-03 23:58:04 +0100

    fix(ResponseParser): Adding more code antests to response parsing

--- src/main/java/dev/nito/xikra/utils/ResponseParser.java
+++ src/main/java/dev/nito/xikra/utils/ResponseParser.java
@@ -1,17 +1,32 @@
 package dev.nito.xikra.utils;
 
 import dev.nito.xikra.dto.XikraState;
+import lombok.experimental.UtilityClass;
+
+import static java.lang.String.format;
 
 /**
  * Given a string as a response, it returns a map with positions to booleans relationships.
  */
+@UtilityClass
 public class ResponseParser {
 
-    XikraState parseBody(String body) throws ResponseParserError {
+    static XikraState parseBody(String body) throws ResponseParserError {
         if (body == null || body.isBlank()) {
             throw new ResponseParserError("Body is null or blank.");
         }
 
+        String[] split = body.split(",");
+        for (String element : split) {
+            checkElementValid(element);
+        }
+
         return null;
+    }
+
+    private static void checkElementValid(String element) throws ResponseParserError {
+        if (!element.equals("0") && !element.equals("1")) {
+            throw new ResponseParserError(format("Element %s is not 0 or 1."));
+        }
     }
 }
--- src/test/java/dev/nito/xikra/utils/ResponseParserTest.java
+++ src/test/java/dev/nito/xikra/utils/ResponseParserTest.java
@@ -2,17 +2,47 @@ package dev.nito.xikra.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ResponseParserTest {
 
-    private final ResponseParser responseParser = new ResponseParser();
 
+    @SuppressWarnings("ConstantConditions")
     @Test
     void shouldThrowExceptionIfNull() {
-
-        assertThatThrownBy(() -> responseParser.parseBody(null)).isInstanceOf(ResponseParserError.class)
+        assertThatThrownBy(() -> ResponseParser.parseBody(null)).isInstanceOf(ResponseParserError.class)
                 .hasMessageContaining("null");
+    }
 
+    @Test
+    void shouldThrowExceptionIfBlank() {
+        assertThatThrownBy(() -> ResponseParser.parseBody("")).isInstanceOf(ResponseParserError.class)
+                .hasMessageContaining("blank");
+    }
+
+    @Test
+    void shouldFailIfNotCommaSeparated() {
+        assertThatThrownBy(() -> ResponseParser.parseBody("1111111")).isInstanceOf(ResponseParserError.class)
+                .hasMessageContaining("");
+    }
+
+    @Test
+    void shouldFailIfItsNotZeroOrOne() {
+        assertThatThrownBy(() -> ResponseParser.parseBody("0,1,3")).isInstanceOf(ResponseParserError.class)
+                .hasMessageContaining("");
+    }
+
+    @Test
+    void shouldReturnAMapForACommaSeparatedEntry() throws ResponseParserError {
+        Map<Integer, Boolean> state = responseParser.parseBody("0,0,0,1,1").getState();
+        assertThat(state).hasSize(5);
+        assertThat(state.get(1)).isFalse();
+        assertThat(state.get(2)).isFalse();
+        assertThat(state.get(3)).isFalse();
+        assertThat(state.get(4)).isTrue();
+        assertThat(state.get(5)).isTrue();
     }
 }
